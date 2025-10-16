c4bff43649e4422546b4050bae02a6cd4578ab75
1t1sCooL <malabugin@anykey.pro>
2025-10-16 21:16:52 +0300

    add styles and adaptive design

--- src/shared/ui/footer/Footer.tsx
+++ src/shared/ui/footer/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
     return (
         <div className={styles.footer}>
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-            Coded by <a href="https://www.frontendmentor.io/profile/1t1sCooL">1t1sCooL</a>.
+            Coded by <a href="https://www.frontendmentor.io/profile/1t1sCooL">1t1sCooL</a>
         </div>
     )
 }
