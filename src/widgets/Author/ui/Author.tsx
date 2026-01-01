dfe916b229d1aa013d3f07ccb84df3246ec7d66a
1t1sCooL <mmalabugin@gmail.com>
2026-01-01 22:16:32 +0300

    ci/cd

--- src/widgets/Author/ui/Author.tsx
+++ src/widgets/Author/ui/Author.tsx
@@ -3,7 +3,7 @@ import styles from "./Author.module.css";
 export const Author = () => {
     return (
         <div className={styles.authorLeft}>
-            <img src={'/avatar-michelle.jpg'} alt="avatar"/>
+            <img src={'/ArticlePreviewComponent/avatar-michelle.jpg'} alt="avatar"/>
             <div className={styles.name}>
                 <h3>Michelle Appleton</h3>
                 <p className={styles.date}>28 Jun 2020</p>
